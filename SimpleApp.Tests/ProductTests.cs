50cc523473a9872e981773f6b5006a16029534b8
chinenye ozoagudike <chinenyeozoagudike@gmail.com>
2021-11-08 13:24:22 +0100

    last commit

---
+++ SimpleApp.Tests/ProductTests.cs
@@ -0,0 +1,37 @@
+﻿using SimpleApp.Models;
+using Xunit;
+
+namespace SimpleApp.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            //Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+
+            //Act
+            p.Name = "New Name";
+
+            //Assert
+            Assert.Equal("New Name", p.Name);
+
+        }
+         
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            // Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+            // Act
+            p.Price = 200M;
+            //Assert
+            Assert.Equal(200M, p.Price);
+        }
+        //Assert.NotEqual(100M, p.Price);
+        //The problem with the unit test is with the arguments to the Assert.
+        //Equal method, which compares the test result to the original 
+        //Price property value rather than the value it has been changed to.
+    }
+}
